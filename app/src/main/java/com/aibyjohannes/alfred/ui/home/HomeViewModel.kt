0aa27924fed63d6e1a94b45d8e57929e4e9b3db1
Johannes Copeland <contact@johannescopeland.com>
2025-12-13 10:52:23 +0100

    feat: Update home screen text
    Changes the welcome text on the home screen from "This is home Fragment" to "My name is A.L.F.R.E.D.".

--- app/src/main/java/com/aibyjohannes/alfred/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/aibyjohannes/alfred/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "My name is A.L.F.R.E.D."
     }
     val text: LiveData<String> = _text
 }
